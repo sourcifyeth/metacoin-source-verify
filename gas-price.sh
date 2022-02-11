e1866438f48332c226dbe1d6d76cc895e45f9dbb
kuzdogan <kaanuzdogan@hotmail.com>
2022-02-11 12:27:02 +0300

    Fix gas price RPC result regex

--- gas-price.sh
+++ gas-price.sh
@@ -6,7 +6,8 @@ fi
 
 TMP=$(\
     curl -X POST https://eth-$CHAIN.alchemyapi.io/v2/$ALCHEMY_ID\
-        --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":42}'\
-    | tac | tac | sed -re 's/^.*"result": "0x(.*)".*$/\1/'\
+        --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":"sourcify"}'\
+    | tac | tac | sed -re 's/^.*"result":"0x(.*)".*$/\1/'\
 )
+
 echo $(( 16#$TMP ))
